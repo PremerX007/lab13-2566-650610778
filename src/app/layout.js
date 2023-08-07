cf87782d61fc3626c78b71f8b223e5733ecd2d83
PremerX007 <39229888+PremerX007@users.noreply.github.com>
2023-08-07 15:25:28 +0700

    completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Bannawat Vongpooton"
+            studentId="650610778"
           />
         </div>
       </body>
